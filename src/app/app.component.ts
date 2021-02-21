e74fb008e07f21a93f8972091d73e5ce44a9475d
afgvivas <linkon90@hotmail.com>
2021-02-20 22:23:10 -0500

    guia1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+export class Equipo{
+  id: number;
+  nombre: string;
+  ciudad: string;
+  director: string;
+}
+
 @Component({
   selector: 'pagl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'misequiposfavoritos';
+  titulo = 'Mis Equipos Favoritos';
+  equipo: Equipo = {
+    id:1,
+    nombre:"america de cali",
+    ciudad:"cali",
+    director:"juanito"
+  }
 }
