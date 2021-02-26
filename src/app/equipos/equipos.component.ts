611c02acbd662afe7a71bdfd6be497a6edadb4c3
afgvivas <linkon90@hotmail.com>
2021-02-26 12:13:13 -0500

    guia 4 terminada

--- src/app/equipos/equipos.component.ts
+++ src/app/equipos/equipos.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Equipo } from '../equipo';
+import { EQUIPOS } from '../lista-equipos';
+
 @Component({
   selector: 'pagl-equipos',
   templateUrl: './equipos.component.html',
   styleUrls: ['./equipos.component.css']
 })
 export class EquiposComponent implements OnInit {
-  equipo: Equipo = {
+  /* equipo: Equipo = {
     id:1,
     nombre:"america de cali",
     ciudad:"cali",
     director:"juanito"
+  } */
+
+  equipos = EQUIPOS;
+
+  equipo: Equipo;
+
+  seleccionar(item: Equipo):void{
+    this.equipo = item;
+    //Mejora de codigo para que oculte los campos en caso de que se de click sobre el que esta seleccionado
+    //!this.equipo ? this.equipo = item : (this.equipo==item? this.equipo = null : this.equipo = item);
   }
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/lista-equipos.ts
@@ -0,0 +1,9 @@
+import { Equipo } from './equipo';
+
+export const EQUIPOS : Equipo[] = [
+    {id: 1, nombre: "Millos", ciudad: "bogota", director: "nu se"},
+    {id: 2, nombre: "America", ciudad: "cali", director: "tampoco se"},
+    {id: 3, nombre: "Real", ciudad: "bogota", director: "peor"},
+    {id: 4, nombre: "Atletico", ciudad: "bogota", director: "ni se diga"},
+    {id: 5, nombre: "Juve", ciudad: "bogota", director: "dorime"},
+];
